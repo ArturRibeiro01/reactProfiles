e1a866428736d303548bcd53fe1b5c47997407ff
Artur Ribeiro <profissional.arturrc@gmail.com>
2022-08-08 00:16:15 -0300

    Conslusão do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from "./Header.module.css";
+import igniteLogo from "../assets/img/Ignite_logo.svg";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <strong>Artur Feed</strong>
+      <img src={igniteLogo} alt="Logomarca" />
     </header>
   );
 };
