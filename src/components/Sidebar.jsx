3725e675bb22b2d42813ab6fc234843d69710b66
Artur Ribeiro <profissional.arturrc@gmail.com>
2022-08-08 09:58:10 -0300

    Feat: Finalização do SideBar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PencilLine } from "phosphor-react";
 
 import styles from "./Sidebar.module.css";
 
@@ -10,13 +11,21 @@ export const Sidebar = () => {
           src="https://images.unsplash.com/photo-1612994370726-5d4d609fca1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=90"
           className={styles.cover}
         />
+
         <div className={styles.profile}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/59667445?v=4"
+            alt="imagem de perfil"
+          />
           <strong>Artur Ribeiro</strong>
           <span>Desenvolvedor Web</span>
         </div>
 
         <footer>
-          <a href="#">Editar ser Perfil</a>
+          <a href="#">
+            <PencilLine size={20} /> Editar ser perfil
+          </a>
         </footer>
       </aside>
     </div>
