4526154af79435ca467b5405107e3a0eecdc4f4c
Artur Ribeiro <profissional.arturrc@gmail.com>
2022-08-15 14:07:30 -0300

    Incluindo Keys faltantes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -22,8 +22,6 @@ export const Post = ({ author, publishedAt, content }) => {
 
   const handleCreateNewComment = (event) => {
     event.preventDefault();
-
-    console.log(event);
     const newCommentText = event.target.comment.value;
 
     setComments([...comments, newCommentText]);
@@ -57,10 +55,10 @@ export const Post = ({ author, publishedAt, content }) => {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="">{line.content}</a>
               </p>
             );
@@ -84,7 +82,7 @@ export const Post = ({ author, publishedAt, content }) => {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment content={comment} />;
+          return <Comment key={comment} content={comment} />;
         })}
       </div>
     </article>
